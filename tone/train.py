d1eca8221da6b929598ad68e1cdde2f3b1bed74e
torbenconto <torbenmconto@gmail.com>
2025-12-14 15:13:49 -0500

    formatting, modify early stopping params

--- tone/train.py
+++ tone/train.py
@@ -3,11 +3,11 @@ from functools import partial
 import torch
 from datasets import Audio, load_dataset
 from transformers import (
+    EarlyStoppingCallback,
     HubertModel,
     Trainer,
     TrainingArguments,
     Wav2Vec2FeatureExtractor,
-    EarlyStoppingCallback
 )
 
 from models.combined import CombinedModel
@@ -52,11 +52,11 @@ def collate_fn_train(batch, feature_extractor):
 if __name__ == "__main__":
     dataset = load_dataset("AbstractTTS/IEMOCAP")
     dataset = dataset.cast_column("audio", Audio(sampling_rate=16000))
-    
-    split = dataset["train"].train_test_split(test_size=0.2, seed = 67)
+
+    split = dataset["train"].train_test_split(test_size=0.2, seed=67)
     dataset = {"train": split["train"], "test": split["test"]}
 
-    val_split = dataset["train"].train_test_split(test_size=0.1, seed = 67)
+    val_split = dataset["train"].train_test_split(test_size=0.1, seed=67)
     dataset["train"] = val_split["train"]
     dataset["validation"] = val_split["test"]
 
@@ -82,7 +82,6 @@ if __name__ == "__main__":
         remove_unused_columns=False,
         dataloader_num_workers=4,
         dataloader_pin_memory=True,
-
         load_best_model_at_end=True,
         eval_strategy="epoch",
         metric_for_best_model="eval_loss",
@@ -93,8 +92,12 @@ if __name__ == "__main__":
         args=training_args,
         train_dataset=dataset["train"],
         data_collator=partial(collate_fn_train, feature_extractor=feature_extractor),
-        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
-        eval_dataset = dataset["validation"],
+        callbacks=[
+            EarlyStoppingCallback(
+                early_stopping_patience=2, early_stopping_threshold=1e-4
+            )
+        ],
+        eval_dataset=dataset["validation"],
     )
 
     trainer.train()
