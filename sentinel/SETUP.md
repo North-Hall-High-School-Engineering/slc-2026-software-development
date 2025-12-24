ffe99c5884985a00a6a9fe1447140d0e4e48e5e2
torbenconto <torbenmconto@gmail.com>
2025-12-24 15:35:03 -0500

    full project boilerplate

--- sentinel/SETUP.md
+++ sentinel/SETUP.md
@@ -5,6 +5,6 @@
 
 ```
   cmake --preset conan-default
+
   cmake --build --preset conan-release
 ```
-https://www.youtube.com/watch?v=kswbIXYBCpo
--- sentinel/src/main.cpp
+++ sentinel/src/main.cpp
@@ -4,9 +4,6 @@
 #include <vector>
 #include <iostream>
 
-constexpr int SAMPLING_RATE = 16000;
-constexpr const char* SOCKET_NAME = "tone-sentinel";
-
 int main() {
     return 0;
 }
